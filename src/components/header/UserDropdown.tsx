b92571d9a152349157cd39752f87274bb2785dc1
Danial B. Yousefi <danialby@gmail.com>
2025-03-04 09:26:06 +0330

    some fixes on appHeader/multiselect/user and notification dropdown/searchbar ||| diloop logo added

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -4,7 +4,9 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { Dropdown } from "../ui/dropdown/Dropdown";
 import { DropdownItem } from "../ui/dropdown/DropdownItem";
-
+import {
+  UserCircleIcon
+} from "../../icons/index";
 export default function UserDropdown() {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -19,19 +21,20 @@ function toggleDropdown(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
   return (
     <div className="relative">
       <button
-        onClick={toggleDropdown} 
+        onClick={toggleDropdown}
         className="flex items-center text-gray-700 dark:text-gray-400 dropdown-toggle"
       >
-        <span className="mr-3 overflow-hidden rounded-full h-11 w-11">
-          <Image
-            width={44}
-            height={44}
-            src="/images/user/owner.jpg"
-            alt="User"
-          />
+        <span className="mr-3 overflow-hidden rounded-full h-11 w-11 flex items-center justify-center">
+          <UserCircleIcon className="h-[26px] w-[26px]" />
+          {/*<Image*/}
+          {/*  width={44}*/}
+          {/*  height={44}*/}
+          {/*  src="/images/user/owner.jpg"*/}
+          {/*  alt="User"*/}
+          {/*/>*/}
         </span>
 
-        <span className="block mr-1 font-medium text-theme-sm">Musharof</span>
+        {/*<span className="block mr-1 font-medium text-theme-sm">Musharof</span>*/}
 
         <svg
           className={`stroke-gray-500 dark:stroke-gray-400 transition-transform duration-200 ${
@@ -56,7 +59,7 @@ function toggleDropdown(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
       <Dropdown
         isOpen={isOpen}
         onClose={closeDropdown}
-        className="absolute right-0 mt-[17px] flex w-[260px] flex-col rounded-2xl border border-gray-200 bg-white p-3 shadow-theme-lg dark:border-gray-800 dark:bg-gray-dark"
+        className="absolute right-auto left-0 mt-[17px] flex w-[260px] flex-col rounded-2xl border border-gray-200 bg-white p-3 shadow-theme-lg dark:border-gray-800 dark:bg-gray-dark"
       >
         <div>
           <span className="block font-medium text-gray-700 text-theme-sm dark:text-gray-400">
--- src/components/skills/SearchBar.tsx
+++ src/components/skills/SearchBar.tsx
@@ -23,7 +23,20 @@ const SearchBar: React.FC = () => {
 <div className="hidden lg:block">
     <form>
         <div className="relative">
-                <span className="absolute -translate-y-1/2 left-4 top-1/2 pointer-events-none">
+
+            <input
+                ref={skillInputRef}
+                type="text"
+                placeholder="جستجو..."
+                className="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-200 bg-white py-2.5 pl-12 pr-10 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-800 dark:bg-gray-900 dark:bg-white/[0.03] dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800 xl:w-[250px]"
+            />
+
+            {/*<button*/}
+            {/*    className="absolute left-2.5 top-1/2 inline-flex -translate-y-1/2 items-center gap-0.5 rounded-lg border border-gray-200 bg-gray-50 px-[7px] py-[4.5px] text-xs -tracking-[0.2px] text-gray-500 dark:border-gray-800 dark:bg-white/[0.03] dark:text-gray-400">*/}
+            {/*    <span> K </span>*/}
+            {/*    <span> ⌘ </span>*/}
+            {/*</button>*/}
+            <span className="absolute -translate-y-1/2 right-4 top-1/2 pointer-events-none">
                   <svg
                       className="fill-gray-500 dark:fill-gray-400"
                       width="20"
@@ -40,17 +53,6 @@ const SearchBar: React.FC = () => {
                     />
                   </svg>
                 </span>
-            <input
-                ref={skillInputRef}
-                type="text"
-                placeholder="جستجو..."
-                className="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-200 bg-transparent py-2.5 pl-12 pr-14 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-800 dark:bg-gray-900 dark:bg-white/[0.03] dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800 xl:w-[430px]"
-            />
-
-            <button className="absolute right-2.5 top-1/2 inline-flex -translate-y-1/2 items-center gap-0.5 rounded-lg border border-gray-200 bg-gray-50 px-[7px] py-[4.5px] text-xs -tracking-[0.2px] text-gray-500 dark:border-gray-800 dark:bg-white/[0.03] dark:text-gray-400">
-                <span> K </span>
-                <span> ⌘ </span>
-            </button>
         </div>
     </form>
 </div>
