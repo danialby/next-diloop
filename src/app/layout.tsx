18bbeb5bc2bd738612c2777f0a601f575907fa59
danialby <danialby@gmail.com>
2025-03-06 18:07:44 +0330

    vazir font got local

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,8 @@
-import { Outfit } from "next/font/google";
+import {vazirfd} from "@/plugins/fonts";
 import "./globals.css";
 
 import { SidebarProvider } from "@/context/SidebarContext";
 import { ThemeProvider } from "@/context/ThemeContext";
-
-const outfit = Outfit({
-  variable: "--font-outfit-sans",
-  subsets: ["latin"],
-});
 
 export default function RootLayout({
   children,
@@ -16,7 +11,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${outfit.variable} fa dark:bg-gray-900`}>
+      <body className={`${vazirfd.variable} dark:bg-gray-900`}>
         <ThemeProvider>
           <SidebarProvider>{children}</SidebarProvider>
         </ThemeProvider>
